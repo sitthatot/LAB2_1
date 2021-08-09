b8eae6d680a919fa3dd1118d95bc8e1a7405bce3
sitthatot <51449357+sitthatot@users.noreply.github.com>
2021-08-09 17:21:08 +0700

    เพิ่มเติม

--- LAB2_1/LAB2_1.cpp
+++ LAB2_1/LAB2_1.cpp
@@ -1,8 +1,24 @@
 #define _CRT_SECURE_NO_WARNINGS 1
-#include<stdio.h>
-int main() {
-	int number;
-	scanf("%d", &number);
-	printf("%d", number);
-	return 0;
+#include <stdio.h>
+int main()
+{
+    int number, i = 2;
+    scanf("%d", &number);
+    while (number != 1)
+    {
+        if (number % i == 0)
+        {
+            printf("%d ", i);
+            number = number / i;
+            if (number != 1)
+            {
+                printf("x ");
+            }
+        }
+        else
+        {
+            i++;
+        }
+    }
+    return 0;
 }
